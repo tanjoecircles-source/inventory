7bd09bc1f81cc6fce1f6ea04575d43dd7f2b2e32
juniarhendra <juniarhendra24@gmail.com>
2026-02-23 13:50:19 +0700

    update login

--- resources/views/core/login.blade.php
+++ resources/views/core/login.blade.php
@@ -76,21 +76,65 @@
                 <button type="submit" class="btn btn-lg btn-primary btn-block"><i class="fe fe-arrow-right"></i> Masuk</button>        
             </div>
             </form>
-            <div class="text-center">
-                {{-- <div class="font-weight-normal fs-15 mb-2">Kamu belum punya akun?</div>
-                <a class="btn btn-block btn-lg btn-outline-primary font-weight-normal" href="{{ url('register') }}"><i class="fe fe-edit-3"></i> Daftar Sekarang</a>
-                 --}}
-                <div class="font-weight-semibold fs-15 mb-2 mt-5">Aceh Gayo Coffee Price List</div>
-                <div class="row mt-2 mb-5">
-                    <div class="col-12">
-                        <a class="btn btn-lg btn-white btn-block mb-2" href="{{ url('greenbeans') }}">Green Beans</a>
+            <div class="font-weight-semibold fs-15 mb-4 mt-5 text-center">Daftar Produk dan Harga<br>Kopi Aceh Gayo</div>
+            <div class="row">
+                <div class="col-6 pr-2">
+                    <div class="card overflow-hidden">
+                        <a href="{{ url('greenbeans') }}" target="_blank"><img src="{{ asset('assets/images/products/greenbean.png') }}" alt="image" class="img-height"></a>
+                        <div class="card-body p-2">
+                            <a href="{{ url('greenbeans') }}" target="_blank" class="card-title mb-1 mt-0 fs-15 font-weight-semibold">Green Bean</a>
+                            <p class="card-text fs-12 text-muted mb-2">Daftar Produk dan Harga Green Beans</p>
+                            <a href="https://wa.me/?text=Berikut%20Daftar%20Harga%20Green%20Bean%20Aceh%20Gayo%20Toko%20Kopi%20Tanjoe%20{{ urlencode('https://app.tanjoecoffee.com/greenbeans') }}" target="_blank" class="btn btn-dark btn-sm"><i class="fe fe-share"></i></a>
+                            <a id="copyurlgreen" class="btn btn-white btn-sm"><i class="fe fe-copy"></i></a>
+                        </div>
                     </div>
-                    <div class="col-12">
-                        <a class="btn btn-lg btn-dark btn-block mb-2" href="{{ url('roastedbeans') }}">Roasted Beans</a>
+                </div>
+                <div class="col-6 pl-2">
+                    <div class="card overflow-hidden">
+                        <a href="{{ url('roastedbeans') }}" target="_blank"><img src="{{ asset('assets/images/products/roasted.png') }}" alt="image" class="img-height"></a>
+                        <div class="card-body p-2">
+                            <a href="{{ url('roastedbeans') }}" target="_blank" class="card-title mb-1 mt-0 fs-15 font-weight-semibold">Roasted Bean</a>
+                            <p class="card-text fs-12 text-muted mb-2">Daftar Produk dan Harga Roasted Beans</p>
+                            <a href="https://wa.me/?text=Berikut%20Daftar%20Harga%20Roasted%20Bean%20Aceh%20Gayo%20Toko%20Kopi%20Tanjoe%20{{ urlencode('https://app.tanjoecoffee.com/roastedbeans') }}" target="_blank" class="btn btn-dark btn-sm"><i class="fe fe-share"></i></a>
+                            <a id="copyurlroasted" class="btn btn-white btn-sm"><i class="fe fe-copy"></i></a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+<script>
+document.getElementById('copyurlgreen').addEventListener('click', async () => {
+    try {
+        await navigator.clipboard.writeText('https://app.tanjoecoffee.com/greenbeans');
+        $(function () {
+            notif({
+                msg: "URL Daftar Harga Berhasil Disalin.",
+                type: "success",
+                position: "center"
+            });
+        });
+    } catch (err) {
+        alert('Gagal menyalin URL.');
+        console.error(err);
+    }
+});
+
+document.getElementById('copyurlroasted').addEventListener('click', async () => {
+    try {
+        await navigator.clipboard.writeText('https://app.tanjoecoffee.com/roastedbeans');
+        $(function () {
+            notif({
+                msg: "URL Daftar Harga Berhasil Disalin.",
+                type: "success",
+                position: "center"
+            });
+        });
+    } catch (err) {
+        alert('Gagal menyalin URL.');
+        console.error(err);
+    }
+});
+</script>
 </x-layouts>
