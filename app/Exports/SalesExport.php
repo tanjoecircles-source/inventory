746dc6d9a9fbb1713b6936d651d726628011d515
juniarhendra <juniarhendra24@gmail.com>
2026-03-04 00:53:13 +0700

    export excel

--- app/Exports/SalesExport.php
+++ app/Exports/SalesExport.php
@@ -3,6 +3,8 @@
 namespace App\Exports;
 
 use App\Models\Sales;
+use App\Models\Setting;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
@@ -10,47 +12,48 @@ class SalesExport implements FromCollection, WithHeadings
 {
     public function collection()
     {
-        return Sales::select(
-            'inv_category',
-            'inv_code',
-            'inv_date',
-            'inv_cust',
-            'inv_hpp',
-            'inv_sub_total',
-            'inv_discount',
-            'inv_expedition',
-            'inv_total',
-            'inv_payment',
-            'inv_status_payment',
-            'inv_payment_date',
-            'inv_payment_type',
-            'inv_status',
-            'inv_desc',
-            'author',
-            'created_at'
-        )->get();
+        $report_period = Setting::first();
+        $data = DB::table('sales_items AS s')
+            ->leftJoin('product AS p', 'p.id', '=', 's.itm_product')
+            ->leftJoin('ref_product_type AS rpt', 'rpt.id', '=', 'p.type')
+            ->leftJoin('sales AS sl', 'sl.id', '=', 's.itm_inv_id')
+            ->select('sl.inv_code',
+                    'sl.inv_date',
+                    'rpt.name AS product_type',
+                    'p.name AS product_name',
+                    's.itm_qty AS qty',
+                    's.itm_price AS price',
+                    's.itm_total AS total',
+                    'p.price_hpp AS hpp')
+            ->where([
+                'sl.inv_status' => 'publish', 
+                ['sl.inv_date', '>=', $report_period->report_date_start],
+                ['sl.inv_date', '<=', $report_period->report_date_end],
+            ])
+            ->orderBy('sl.inv_date', 'DESC')
+            ->orderBy('sl.id', 'DESC')
+            ->get();
+
+        foreach ($data as $key => $value) {
+            $value->hpp = $value->hpp * $value->qty;
+            $value->margin = $value->total - $value->hpp;
+        }
+
+        return $data;
     }
 
     public function headings(): array
     {
         return [
-            'Category',
-            'Invoice Code',
-            'Invoice Date',
-            'Customer',
-            'HPP',
-            'Sub Total',
-            'Discount',
-            'Expedition',
+            'Kode Invoice',
+            'Tanggal',
+            'Kategori',
+            'Barang',
+            'Qty',
+            'Harga',
             'Total',
-            'Payment',
-            'Payment Status',
-            'Payment Date',
-            'Payment Type',
-            'Status',
-            'Description',
-            'Author',
-            'Created At'
+            'HPP',
+            'Margin'
         ];
     }
 }
