6aff6cfb82c848ba333bd87c5b78324058ce47a1
juniarhendra <juniarhendra24@gmail.com>
2026-07-08 23:32:19 +0700

    change margin

--- resources/views/web/admin/product/sales-history-paginate.blade.php
+++ resources/views/web/admin/product/sales-history-paginate.blade.php
@@ -1,7 +1,7 @@
 @foreach ($contents as $item)
 <div class="d-flex align-items-center py-3 border-top">
     <div class="flex-grow-1">
-        <h6 class="mb-1 font-weight-semibold">{{ $item->customer_name ?? '-' }}</h6>
+        <h6 class="mb-0 font-weight-semibold">{{ $item->customer_name ?? '-' }}</h6>
         <div class="d-flex justify-content-between align-items-center">
             <small class="text-muted">
                 <i class="fe fe-calendar mr-1"></i>{{ \Carbon\Carbon::parse($item->inv_date)->format('d M Y') }}
@@ -10,7 +10,7 @@
                 <i class="fe fe-shopping-cart mr-1"></i>{{ $item->itm_qty }} pcs
             </span>
         </div>
-        <small class="text-muted d-block mt-1">
+        <small class="text-muted d-block mt-0">
             <i class="fe fe-user mr-1"></i>Author : {{ $item->author_name ?? '-' }}
         </small>
     </div>
