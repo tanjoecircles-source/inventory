3a79906c240ae014efa4b32fc9e8b353846349db
juniarhendra <juniarhendra24@gmail.com>
2026-09-10 00:17:37 +0700

    Perbaikan invoice

--- resources/views/web/admin/sales/print.blade.php
+++ resources/views/web/admin/sales/print.blade.php
@@ -69,24 +69,24 @@
         </table>
         <table class="layout invoice-info">
             <tr>
-                <td width="60%"><b>DITAGIH KEPADA</b></td>
-                <td width="25%" class="text-right"><b>Invoice #</b></td>
+                <td width="60%"><b>Bill To</b></td>
+                <td width="25%" class="text-right"><b>Invoice No #</b></td>
                 <td width="15%" class="text-right">{{$detail->inv_code}}</td>
             </tr>
             <tr>
                 <td>{{$detail->cust_name}}</td>
-                <td class="text-right"><b>Tanggal</b></td>
+                <td class="text-right"><b>Invoice Date</b></td>
                 <td class="text-right">{{date('d M Y', strtotime($detail->inv_date))}}</td>
             </tr>
         </table>
         <table class="table">
             <tr>
-                <td width="44%"><b>Item Barang</b></td>
-                <td width="14%" class="text-right"><b>Harga<br>Retail</b></td>
-                <td width="14%" class="text-right"><b>Item<br>Diskon</b></td>
-                <td width="14%" class="text-right"><b>Harga<br>Akhir</b></td>
-                <td width="10%" class="text-center"><b>Item Qty</b></td>
-                <td width="14%" class="text-right"><b>Jumlah</b></td>
+                <td width="44%"><b>Product</b></td>
+                <td width="14%" class="text-right"><b>Unit Price</b></td>
+                <td width="14%" class="text-right"><b>Discount</b></td>
+                <td width="14%" class="text-right"><b>Fixed Price</b></td>
+                <td width="10%" class="text-center"><b>Qty</b></td>
+                <td width="14%" class="text-right"><b>Amount</b></td>
             </tr>
             @foreach ($item as $item)
             <tr>
@@ -102,26 +102,26 @@
         </table>
         <table class="layout">
             <tr>
-                <td width="50%" rowspan="6" style="vertical-align:bottom"><b>Instruksi Pembayaran</b><p style="margin-top:6px;margin-bottom:0px">Transfer Bank melalui No. Rekening berikut :</p></td>
-                <td width="35%" class="text-right">Total Harga Retail</td>
+                <td width="50%" rowspan="6" style="vertical-align:bottom"><b>Payment Instructions</b><p style="margin-top:6px;margin-bottom:0px">Please make a bank transfer to the following Account Number :</p></td>
+                <td width="35%" class="text-right">Subtotal</td>
                 <td width="15%" class="text-right">Rp {{str_replace(",", ".", number_format($total_before_discount))}}</td>
             </tr>
             <tr>
-                <td class="text-right" style="color:#008000">Total Diskon</td>
+                <td class="text-right" style="color:#008000">Discount</td>
                 <td class="text-right" style="color:#008000">- Rp {{str_replace(",", ".", number_format($total_item_disc))}}</td>
             </tr>
             <tr>
-                <td class="text-right">Total Harga Akhir</td>
+                <td class="text-right">Net Subtotal</td>
                 <td class="text-right">Rp {{str_replace(",", ".", number_format($detail->inv_sub_total))}}</td>
             </tr>
             @if(!empty($detail->inv_expedition))
             <tr>
-                <td class="text-right" style="color:#b70909">Biaya Pengiriman</td>
+                <td class="text-right" style="color:#b70909">Shipping</td>
                 <td class="text-right" style="color:#b70909">+ Rp {{str_replace(",", ".", number_format($detail->inv_expedition))}}</td>
             </tr>
             @endif
             <tr>
-                <td class="text-right"><b>Total Tagihan</b></td>
+                <td class="text-right"><b>TOTAL</b></td>
                 <td class="text-right"><b>Rp {{str_replace(",", ".", number_format($detail->inv_total))}}</b></td>
             </tr>
         </table>
@@ -129,14 +129,14 @@
             <tr>
                 <td width="35%">
                     <img src="{{public_path('/assets/images/png/bca.png')}}" class="icon-blue" style="height:1.5rem;" alt=""><br>
-                    A.N. Muhammad Rizki Awayna<br> No Rekening: <b>7780 106578</b>
+                    On behalf of Muhammad Rizki Awayna<br>Account Number : <b>7780 106578</b>
                 </td>
                 <td width="65%" class="text-right">
                     @if($detail->inv_status_payment == 'unpaid')
-                    Jumlah yang Harus Dibayar
+                    Total Payment Due<br>
                     <h2>Rp {{str_replace(",", ".", number_format($detail->must_pay))}}</h2>
                     @else
-                    <h4 style="color:#008000">LUNAS</h4>
+                    <h4 style="color:#008000">PAID</h4>
                     <h2 style="color:#008000">Rp {{str_replace(",", ".", number_format($detail->inv_payment))}}</h2>
                     @endif
                 </td>
@@ -144,7 +144,7 @@
             <tr>
                 <td>
                     <img src="{{public_path('/assets/images/png/mandiri.png')}}" class="icon-blue" style="height:1.5rem;" alt=""><br>
-                    A.N. Suhendra Juniar Azhari<br> No Rekening: <b>1370011890809</b>
+                    On behalf of Suhendra Juniar Azhari<br>Account Number : <b>1370011890809</b>
                 </td>
                 <td class="text-right">
                     @if(!empty($detail->inv_desc))
